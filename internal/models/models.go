66d7b56e7028a77d160f79647f7762abb2dec0b0
k10wl <aleks.antonsheko@gmail.com>
2024-01-17 23:37:58 +0200

    feat: folders and media models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,5 +1,13 @@
 package models
 
+import "time"
+
+type Meta struct {
+	CreatedAt time.Time `firestore:"createdAt" json:"createdAt,omitempty"`
+	UpdatedAt time.Time `firestore:"updatedAt" json:"updatedAt,omitempty"`
+	DeletedAt time.Time `firestore:"deletedAt" json:"deletedAt,omitempty"`
+}
+
 type Link struct {
 	ID   string
 	URL  string `firestore:"url" json:"url"`
@@ -13,4 +21,21 @@ type Contacts struct {
 
 type About struct {
 	Bio string `firestore:"bio"`
+}
+
+type Folder struct {
+	ID   string
+	Name string `firestore:"name" json:"name,omitempty"`
+	// ROOT or valid ID of other folder
+	ParentID string `firestore:"parentId" json:"parentId,omitempty"`
+	// Media is copied over on folder level because fIrEbAsE fIrEsToRe reasons.
+	Media []Media `firestore:"media" json:"media,omitempty"`
+	Meta
+}
+
+type Media struct {
+	ID string
+	// Storage URL
+	URL string
+	Meta
 }
